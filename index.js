6405256ac00718d9afcc3ab4e45b93d735f51582
PhucIT <lehuuphuc23031999@gmail.com>
2020-01-20 21:51:05 +0700

    caculater

---
+++ index.js
@@ -0,0 +1,22 @@
+function insert(num){
+  //  document.form.textview.value = document.form.textview.value + num;
+    document.getElementsByClassName("textview").value = 1;
+    console.log("hello");
+}
+
+function equal(){
+    let exp = document.form.textview.value;
+    if(exp){
+        document.form.textview.value = eval(exp);
+    }
+}
+
+function clean(){
+    document.form.textview.value = "";
+
+}
+
+function back(){
+    let exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0,exp.length-1);
+}
